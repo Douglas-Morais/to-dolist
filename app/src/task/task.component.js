0c8c617d49db50a1bf250fc03cc0b0fb2e6c9b92
Douglas Morais <douglascarvalhomorais@hotmail.com>
2021-09-19 16:37:36 -0300

    fixedd hover effect in hand icon

--- app/src/task/task.component.js
+++ app/src/task/task.component.js
@@ -18,7 +18,7 @@ export class AppTask extends HTMLElement {
     this.idTask = this.getAttribute('id');
     this.build();
   }
-  
+
 
   build() {
     const template = document.getElementById('task-template');
@@ -40,6 +40,7 @@ export class AppTask extends HTMLElement {
     this.buttonRemove ? this.buttonRemove.addEventListener('click', this.removeTask.bind(this)) : null;
 
     this.buttonEdit = this.shadowRoot.getElementById('edit');
+    this.buttonEdit ? this.buttonEdit.addEventListener('click', this.editTask.bind(this)) : null;
 
   }
 
@@ -53,16 +54,33 @@ export class AppTask extends HTMLElement {
     elTask.classList.remove('task-check-hover');
   }
 
+  editTask() {
+    if(!this.inputDescription.hasAttribute('checked')) {
+      this.inputDescription.removeAttribute('readonly');
+      this.inputDescription.select();
+      this.inputDescription.style.borderBottomColor = 'var(--main-white)';
+      this.inputDescription.style.color = 'var(--main-white)';
+      this.inputDescription.onkeydown = ({key, ...event}) => {
+        if(key === 'Enter') {
+          this.inputDescription.blur();
+          this.inputDescription.style.borderBottomColor = 'var(--main-yellow)';
+          this.inputDescription.style.color = 'var(--main-yellow)';
+        }
+      };
+    }
+  }
+
   checkTask() {
     this.inputDescription.setAttribute('checked', '');
     this.buttonCheck.setAttribute('src', this.pathIconDoubleCheck);
     this.buttonEdit.setAttribute('src', this.pathIconHandOk);
+    this.buttonEdit.classList.remove('btn-edit');
   }
 
   removeTask() {
     const elTask = this.shadowRoot.getElementById('task');
     const taskList = document.getElementById('list-nav');
-    elTask.classList.add('fade-leave');    
+    elTask.classList.add('fade-leave');
     elTask.addEventListener('transitionend', () => this.remove());
   }
 
